b11ee25b27803e9087a93879f235866939c9fc5a
Douglas Uretsky <douglasuretsky@gmail.com>
2018-11-13 15:05:14 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -18,7 +18,7 @@ class Application
       resp.write @@cart.empty? ? "Your cart is empty" : @@cart.join("\n")
       elsif req.path.match(/add/)
       item = req.params["item"]
-      resp.write @@items.include(item) ? @@cart << item
+      resp.write @@items.include(item) ? @@cart << item : 
     else
       resp.write "Path Not Found"
     end
